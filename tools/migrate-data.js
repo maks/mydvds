58cde569b8db409d01a680703e4b301fa6b61542
Maksim Lin <maks@manichord.com>
2011-10-26 14:31:24 +1100

    user and dvd data import working

--- tools/migrate-data.js
+++ tools/migrate-data.js
@@ -1,17 +1,56 @@
+/*global node:true */
+/*jshint globalstrict:true,  sub:true */
+
 /*  simple script to migrate dat from old version of Mydvds
  *  expects data files to be csv format and in subfolder named 'data'
  */
 
+"use strict";
+
 var csv = require('csv'),
+    redis = require('redis'),
+    client = redis.createClient(),
     type = process.argv[2], //first arg on cli
-    loaders = {};
+    loaders = {},
+    maxID = 0;
 
 
-loaders.users = function (data) {
-    console.log("loading user:"+data['EMAIL']);
+function prepID(data) {
+    var id = parseInt(data['id'], 10),
+        i;
+    delete data['ID'];
+
+    if (isNaN(id)) {
+        throw new Error("Invalid "+type+"ID"+data['id']);
+    }
+
+    if (id > maxID) {
+        maxID = id;
+    }
+    return id;
 }
 
-console.log(this.loadusers);
+loaders.users = function (data) {
+    var id = prepID(data),
+        i;
+
+    console.log("loading user:"+data['email']);
+    client.hmset("user:"+id, data);
+    client.sadd("users_all", id);
+};
+
+loaders.dvds = function(data) {
+    var id = prepID(data),
+        i;
+
+    delete data['type'];
+    delete data['ref_id'];
+    delete data['verified'];
+
+    console.log("loading dvd:"+data['title']);
+    client.hmset("dvd:"+id, data);
+    client.sadd("dvds_all", id);
+};
 
 csv()
 .fromPath('data/'+type+'.csv', { "columns" : true} )
@@ -20,7 +59,10 @@ csv()
     loaders[type](data);
 })
 .on('end',function(count){
-    console.log('Number of lines: '+count);
+    console.log('Processed: '+count+' '+type);
+    client.set(type+"_maxid", maxID);
+    client.quit();
+    process.exit(0);
 })
 .on('error',function(error){
     console.log(error.message);
