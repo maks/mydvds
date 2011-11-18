4c1a447d5a9b1fee950cc6be353e10e5f5c3ccab
Maksim <maks@ubuntu.localhost.62.com>
2011-11-18 12:08:34 +1100

    fixed prod paths

--- code/app.js
+++ code/app.js
@@ -26,15 +26,15 @@ app.configure('development', function(){
 app.configure('production', function() {
     app.use(express.logger());
     app.use(express.bodyParser());
-    app.use(express.static('/var/www/mydvds.com.au/html'));
+    app.use(express.static('static'));
     PORT = 8080;
-    BLANK_COVER_PREFIX = '/var/www/mydvds.com.au/icons/blank-cover-';
+    BLANK_COVER_PREFIX = '/var/www/mydvds.com.au/app/static/icons/blank-cover-';
 });
 
 log.info("serving static from:"+ 'static');
 
 app.get('/', function(req, res, next){
-    res.redirect('/home.html');
+    res.redirect('/index.html');
 });
 
 app.get('/api', function(req, res, next){
