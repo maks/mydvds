caed5fdb466d253955646137b4fbaf202a3f720b
nodejs <nodejs@manichord.localhost.62.com>
2011-11-30 16:57:48 +1100

    fixed covers apth.

--- bin/prod.sh
+++ bin/prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export PORT=8080
-export BLANK_COVER_PREFIX="/var/www/mydvds.com.au/app/static/icons/blank-cover-"
+export BLANK_COVER_PREFIX="/var/www/mydvds/app/static/icons/blank-cover-"
 cd /var/www/mydvds/app
 node code/server.js 2>>  /var/log/nodejs/mydvds-error.log 1>> /var/log/nodejs/mydvds.log
