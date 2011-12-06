57d0f039afeee931b0039deb53b026093b0d5ce9
Maksim Lin <maks@manichord.com>
2011-12-06 14:58:23 +1100

    updated to match new usage of node-github-deployhook for deploy paths as github repo names.

--- bin/prod.sh
+++ bin/prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export PORT=8080
-export BLANK_COVER_PREFIX="/var/www/mydvds/app/static/icons/blank-cover-"
-cd /var/www/mydvds/app
+export BLANK_COVER_PREFIX="/var/www/mydvds/static/icons/blank-cover-"
+cd /var/www/mydvds
 node lib/server.js 2>>  /var/log/nodejs/mydvds-error.log 1>> /var/log/nodejs/mydvds.log
